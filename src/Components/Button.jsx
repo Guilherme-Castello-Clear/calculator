b083467f5986dcef86d19f2073922a6c4ea0cd0e
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-03-24 10:43:33 -0300

    Feat: Added display and improved buttons

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -2,5 +2,14 @@ import React from 'react'
 import './Button.css'
 
 export default props => {
-    return <button className='button'>{props.label}</button>
+    let classes = 'button '
+    classes += props.operations ? 'operation' : '';
+    classes += props.double ? 'double' : '';
+    classes += props.triple ? 'triple' : '';
+    console.log(classes);
+    return(
+        <button className={classes} onClick={e => props.click && props.click(props.label)}>
+            {props.label}
+        </button>
+    )
 }
